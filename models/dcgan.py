233afe77e612cc81d5d9da811921877970a27040
alionsss <alionsss@foxmail.com>
2023-07-15 18:46:42 +0800

    调整GeneratorNet变量命名

--- models/dcgan.py
+++ models/dcgan.py
@@ -13,25 +13,25 @@ class GeneratorNet(BasicModule):
         """
         super(GeneratorNet, self).__init__()
 
-        def cnn_block(ins, outs, kernel_size, stride, padding):
+        def tcnn_block(ins, outs, kernel_size, stride, padding):
             return nn.Sequential(
                 nn.ConvTranspose2d(ins, outs, kernel_size, stride, padding, bias=False),
                 nn.BatchNorm2d(outs),
                 nn.ReLU(True)
             )
 
-        self.cnn_block1 = cnn_block(nz, ngf * 8, 4, 1, 0)  # (ngf*8) x 4 x 4
-        self.cnn_block2 = cnn_block(ngf * 8, ngf * 4, 4, 2, 1)  # (ngf*4) x 8 x 8
-        self.cnn_block3 = cnn_block(ngf * 4, ngf * 2, 4, 2, 1)  # (ngf*2) x 16 x 16
-        self.cnn_block4 = cnn_block(ngf * 2, ngf, 4, 2, 1)  # (ngf) x 32 x 32
-        self.cnn_block5 = nn.Sequential(nn.ConvTranspose2d(ngf, 3, 5, 3, 1, bias=False), nn.Tanh())  # 3 x 96 x 96
+        self.tcnn_block1 = tcnn_block(nz, ngf * 8, 4, 1, 0)  # (ngf*8) x 4 x 4
+        self.tcnn_block2 = tcnn_block(ngf * 8, ngf * 4, 4, 2, 1)  # (ngf*4) x 8 x 8
+        self.tcnn_block3 = tcnn_block(ngf * 4, ngf * 2, 4, 2, 1)  # (ngf*2) x 16 x 16
+        self.tcnn_block4 = tcnn_block(ngf * 2, ngf, 4, 2, 1)  # (ngf) x 32 x 32
+        self.tcnn_block5 = nn.Sequential(nn.ConvTranspose2d(ngf, 3, 5, 3, 1, bias=False), nn.Tanh())  # 3 x 96 x 96
 
     def forward(self, X):
-        out = self.cnn_block1(X)
-        out = self.cnn_block2(out)
-        out = self.cnn_block3(out)
-        out = self.cnn_block4(out)
-        out = self.cnn_block5(out)
+        out = self.tcnn_block1(X)
+        out = self.tcnn_block2(out)
+        out = self.tcnn_block3(out)
+        out = self.tcnn_block4(out)
+        out = self.tcnn_block5(out)
         return out
 
 
