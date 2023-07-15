5636d3974dac2337f2ad7d89f8ddb504e13b1728
alionsss <alionsss@foxmail.com>
2023-07-15 20:17:48 +0800

    完善代码

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
+import inspect
+import os
+
+import fire
 import torch
 import torchvision
-from data import AnimeFaces128Dataset
-from torch.utils.data import DataLoader
-from torchnet.meter import AverageValueMeter
-import fire
 import tqdm
-
-import os
-import inspect
+from torch.utils.data import DataLoader
+from torch.nn import functional as F
+from torchnet.meter import AverageValueMeter
 
 from config import ExtDefaultConfig, DefaultConfig
+from data import AnimeFaces128Dataset
 from models import GeneratorNet, DiscriminatorNet
 
 opt = ExtDefaultConfig()
@@ -60,18 +61,18 @@ def train(**kwargs):
                      fix_noises, noises, vis)
 
         if epoch % opt.save_every == 0:
-            with torch.no_grad():
-                # 保存图片、模型
-                fix_fake_imgs = net_g(fix_noises)
-                if not os.path.exists(opt.train_save_path) or not os.path.isdir(opt.train_save_path):
-                    os.mkdir(opt.train_save_path)
-                file_path = os.path.join(opt.train_save_path, str(epoch) + ".png")
-                torchvision.utils.save_image(fix_fake_imgs.data[:64], file_path, normalize=True, value_range=(-1, 1))
-                net_g.save('GeneratorNet_%s.pth' % epoch)
-                net_d.save('DiscriminatorNet_%s.pth' % epoch)
+            # 保存图片、模型
+            fix_fake_imgs = net_g(fix_noises).detach()
+            if not os.path.exists(opt.train_save_path) or not os.path.isdir(opt.train_save_path):
+                os.mkdir(opt.train_save_path)
+            file_path = os.path.join(opt.train_save_path, str(epoch) + ".png")
+            torchvision.utils.save_image(fix_fake_imgs.data[:64], file_path, normalize=True, value_range=(-1, 1))
 
-                error_meter_g.reset()
-                error_meter_d.reset()
+            net_g.save('GeneratorNet_%s.pth' % epoch)
+            net_d.save('DiscriminatorNet_%s.pth' % epoch)
+
+            error_meter_g.reset()
+            error_meter_d.reset()
 
 
 def _train_epoch(epoch_flag, net_d, net_g, optimizer_d, optimizer_g, dataloader, error_meter_d, error_meter_g,
@@ -80,46 +81,48 @@ def _train_epoch(epoch_flag, net_d, net_g, optimizer_d, optimizer_g, dataloader,
     for i, (img, _) in enumerate(progress, 1):
         real_img = img.to(opt.device)
 
+        # 训练判别器
         if i % opt.d_every == 0:
-            # 训练判别器
             optimizer_d.zero_grad()
-            # 尽可能的把真图片判别为正确,尽可能把假图片判别为错误
-            r_preds = net_d(real_img)
 
+            # 尽可能的把真图片判别为正确，尽可能把假图片判别为错误
+            r_preds = net_d(real_img)
             noises.data.copy_(torch.randn(opt.batch_size, opt.nz, 1, 1))
-            fake_img = net_g(noises).detach()  # 根据噪声生成假图
+            fake_img = net_g(noises).detach()  # 现在不训练生成器
             f_preds = net_d(fake_img)
-            # 参考Relativistic average HingeGAN
+
+            # 计算损失
             r_f_diff = (r_preds - f_preds.mean()).clamp(max=1)
             f_r_diff = (f_preds - r_preds.mean()).clamp(min=-1)
-            loss_d_real = (1 - r_f_diff).mean()
-            loss_d_fake = (1 + f_r_diff).mean()
-            loss_d = loss_d_real + loss_d_fake
+            loss_d = (1 - r_f_diff).mean() + (1 + f_r_diff).mean()
 
             loss_d.backward()
             optimizer_d.step()
             error_meter_d.add(loss_d.item())
 
+        # 训练生成器
         if i % opt.g_every == 0:
-            # 训练生成器
             optimizer_g.zero_grad()
+
+            # 尽可能提高生成的图像被判别为真的概率
             noises.data.copy_(torch.randn(opt.batch_size, opt.nz, 1, 1))
             fake_img = net_g(noises)
             f_preds = net_d(fake_img)
             r_preds = net_d(real_img)
 
-            r_f_diff = r_preds - torch.mean(f_preds)
-            f_r_diff = f_preds - torch.mean(r_preds)
+            # 计算损失
+            r_f_diff = F.relu(r_preds - f_preds.mean())
+            f_r_diff = F.relu(f_preds - r_preds.mean())
+            error_g = (1 + r_f_diff).mean() + (1 - f_r_diff).mean()
 
-            error_g = torch.mean(torch.nn.ReLU()(1 + r_f_diff)) + torch.mean(torch.nn.ReLU()(1 - f_r_diff))
             error_g.backward()
             optimizer_g.step()
             error_meter_g.add(error_g.item())
 
         if opt.vis and i % opt.plot_every == 0:
             # 可视化
-            fix_fake_imgs = net_g(fix_noises)
-            vis.images(fix_fake_imgs.detach().cpu().numpy()[:64] * 0.5 + 0.5, win='fixfake')
+            fix_fake_imgs = net_g(fix_noises).detach()
+            vis.images(fix_fake_imgs.cpu().numpy()[:64] * 0.5 + 0.5, win='fixfake')
             vis.images(real_img.data.cpu().numpy()[:64] * 0.5 + 0.5, win='real')
             vis.plot('error_g', error_meter_g.value()[0])
             vis.plot('error_d', error_meter_d.value()[0])
@@ -137,15 +140,13 @@ def generate(**kwargs):
     net_g, net_d = _get_model()
     net_g.eval(), net_d.eval()
 
-    noises = torch.randn(opt.gen_search_num, opt.nz, 1, 1).normal_(opt.gen_mean, opt.gen_std)
-    noises = noises.to(opt.device)
+    noises = torch.randn(opt.gen_search_num, opt.nz, 1, 1).normal_(opt.gen_mean, opt.gen_std).to(opt.device)
 
     # 生成图片，并计算图片在判别器的分数
     fake_img = net_g(noises)
     scores = net_d(fake_img).detach()
 
     # 挑选最好的某几张
-    print(scores.shape)
     indexs = scores.topk(opt.gen_num)[1]
     result = []
     for i in indexs:
